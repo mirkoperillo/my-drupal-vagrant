9c92fad154fdd3ff91914a76519b60a46572a921
mirkoperillo <mirko.perillo@gmail.com>
2017-05-03 15:54:37 +0200

    fix apache conf to support clean urls

--- setup-apache.sh
+++ setup-apache.sh
@@ -7,4 +7,6 @@ if ! [ -L /var/www ]; then
   ln -fs /vagrant/www /var/www
 fi
 
+a2enmod rewrite
 perl -0 -i.bak  -p -e 's/\s*<Directory \/var\/www\/>\n\s+Options\sIndexes\sFollowSymLinks\sMultiViews\n\s+AllowOverride\sNone/\n        <Directory \/var\/www\/>\n                Options Indexes FollowSymLinks MultiViews\n                AllowOverride All/g;' /etc/apache2/sites-enabled/000-default
+apache2ctl restart
