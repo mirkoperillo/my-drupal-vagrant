57bf760029c95d9661ebc50d96f5128d3ba91dc7
mirkoperillo <mirko.perillo@gmail.com>
2017-05-03 13:42:27 +0200

    fix #1

--- setup-apache.sh
+++ setup-apache.sh
@@ -6,3 +6,5 @@ if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant/www /var/www
 fi
+
+perl -0 -i.bak  -p -e 's/\s*<Directory \/var\/www\/>\n\s+Options\sIndexes\sFollowSymLinks\sMultiViews\n\s+AllowOverride\sNone/\n        <Directory \/var\/www\/>\n                Options Indexes FollowSymLinks MultiViews\n                AllowOverride All/g;' /etc/apache2/sites-enabled/000-default
